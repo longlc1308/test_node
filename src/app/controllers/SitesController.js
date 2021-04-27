da05b706949af16d1fab140516e239e34bc6252f
longlc1308 <longluucong1308@gmail.com>
2021-04-27 10:43:24 +0700

    routes, app/controller

---
+++ src/app/controllers/SitesController.js
@@ -0,0 +1,11 @@
+class SitesController {
+    // Get /news
+    index(req, res) {
+            res.render('home');
+        }
+        // Get /news/:slug
+    search(req, res) {
+        res.render('search');
+    }
+}
+module.exports = new SitesController;
